5131cd5c95bb649429eb3180909fb4e6559fea9e
Muri <mcsanchezlaguna@gmail.com>
2025-02-25 15:25:34 +0100

    acciones añadidas a la imagen

--- SRC/index.js
+++ SRC/index.js
@@ -1,16 +1,23 @@
-/* Mostrar y ocultar detalles */
 document.addEventListener("DOMContentLoaded", function () {
-  buttons = document.querySelectorAll(".show-more");
+  const buttons = document.querySelectorAll(".show-more");
 
   buttons.forEach((button) => {
     button.addEventListener("click", function () {
       const details = this.previousElementSibling;
+      const image = this.parentElement.querySelector(".timeline-image");
+
       if (details.style.display === "none" || details.style.display === "") {
         details.style.display = "block";
         this.textContent = "Mostrar menos";
+        if (image) {
+          image.style.display = "none"; // Oculta la imagen
+        }
       } else {
         details.style.display = "none";
         this.textContent = "Mostrar más";
+        if (image) {
+          image.style.display = "block"; // Vuelve a mostrar la imagen
+        }
       }
     });
   });
